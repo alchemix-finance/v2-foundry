8f33ae98432e1a024aa446fee8e018eddca87a1f
0xtao <0xtao@protonmail.com>
2022-03-19 03:00:48 -0600

    SafeERC20: Actually added SafeERC20

---
+++ src/test/utils/users/ERC20User.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+
+contract ERC20User {
+    IERC20 private token;
+
+    constructor(IERC20 _token) {
+        token = _token;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        return token.approve(spender, amount);
+    }
+
+    function transfer(address receiver, uint256 amount) external returns (bool) {
+        return token.transfer(receiver, amount);
+    }
+
+    function transferFrom(address owner, address receiver, uint256 amount) external returns (bool) {
+        return token.transferFrom(owner, receiver, amount);
+    }
+}
