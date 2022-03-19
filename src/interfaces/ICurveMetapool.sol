5532eb6c43eb81e3f8c24b69ce41d3155b566bda
0xfoobar <0xfoobar1@gmail.com>
2022-03-19 12:40:01 -0400

    Address comments

--- src/interfaces/ICurveMetapool.sol
+++ src/interfaces/ICurveMetapool.sol
@@ -1,15 +1,13 @@
 pragma solidity 0.8.13;
 
 interface ICurveMetapool {
-    /**
-    @notice Perform an exchange between two underlying coins
-    @dev Index values can be found via the `underlying_coins` public getter method
-    @param i Index value for the underlying coin to send
-    @param j Index valie of the underlying coin to recieve
-    @param dx Amount of `i` being exchanged
-    @param min_dy Minimum amount of `j` to receive
-    @return Actual amount of `j` received
-    */
+    /// @notice Perform an exchange between two underlying coins
+    /// @dev Index values can be found via the `underlying_coins` public getter method
+    /// @param i Index value for the underlying coin to send
+    /// @param j Index valie of the underlying coin to recieve
+    /// @param dx Amount of `i` being exchanged
+    /// @param min_dy Minimum amount of `j` to receive
+    /// @return Actual amount of `j` received
     function exchange_underlying(int128 i, int128 j, uint256 dx, uint256 min_dy) external returns (uint256);
 }
 
