bf76b9750fef373ae5cc0e35d32a5b5204ede706
hesnicewithit <splitthedutch@gmail.com>
2022-04-11 16:09:06 -0600

    Changed unwra and wrap tests into single roundabout tests. Added fuzzing

--- src/test/FuseTokenAdapterV1.t.sol
+++ src/test/FuseTokenAdapterV1.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.13;
 
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
 import {stdCheats} from "forge-std/stdlib.sol";
 import "forge-std/console.sol";
@@ -16,9 +18,10 @@ import {SafeERC20} from "../libraries/SafeERC20.sol";
 import {LibFuse} from "../libraries/LibFuse.sol";
 
 contract FuseTokenAdapterV1Test is DSTestPlus, stdCheats {
+    ICERC20 fDAI = ICERC20(0x7e9cE3CAa9910cc048590801e64174957Ed41d43);
     FuseTokenAdapterV1 adapter;
 
-    ICERC20 fDAI = ICERC20(0x7e9cE3CAa9910cc048590801e64174957Ed41d43);
+    uint256 constant BPS = 10000;
 
     function setUp() external {
         adapter = new FuseTokenAdapterV1(AdapterInitializationParams({
@@ -34,23 +37,44 @@ contract FuseTokenAdapterV1Test is DSTestPlus, stdCheats {
         assertApproxEq(adapter.price(), fDAI.exchangeRateStored(), 1e14);
     }
 
-    function testWrap() external {
-        tip(address(adapter.underlyingToken()), address(this), 1e18);
+    function testUnwrap() external {
+        uint256 amount = 1e18;
 
-        SafeERC20.safeApprove(adapter.underlyingToken(), address(adapter), 1e18);
+        tip(adapter.underlyingToken(), address(this), amount);
 
-        uint256 wrapped = adapter.wrap(1e18, address(0xbeef));
+        SafeERC20.safeApprove(adapter.underlyingToken(), address(adapter), amount);
 
-        assertApproxEq(1e18 , wrapped * adapter.price() / 1e18, 1e10);
+        uint256 wrapped = adapter.wrap(amount, address(this));
+
+        assertGt(wrapped * adapter.price(), (amount * 9900 / BPS));
+        
+        SafeERC20.safeApprove(adapter.token(), address(adapter), wrapped);
+
+        uint256 unwrapped = adapter.unwrap(wrapped, address(0xbeef));
+        
+        assertGt(unwrapped, amount * 9900 / BPS);
+        assertGt(IERC20(adapter.underlyingToken()).balanceOf(address(0xbeef)), amount * 9900 / BPS);
     }
 
-    function testUnwrap() external {
-        tip(adapter.token(), address(this), 1e18);
-
-        SafeERC20.safeApprove(adapter.token(), address(adapter), 1e18);
-
-        uint256 unwrapped = adapter.unwrap(1e18, address(0xbeef));
+    function testUnwrap(uint256 amount) external {
+        hevm.assume(
+            amount >= 10**SafeERC20.expectDecimals(adapter.underlyingToken()) && 
+            amount < type(uint96).max
+        );
         
-        assertApproxEq(adapter.price() , unwrapped, 1e10);
+        tip(adapter.underlyingToken(), address(this), amount);
+
+        SafeERC20.safeApprove(adapter.underlyingToken(), address(adapter), amount);
+
+        uint256 wrapped = adapter.wrap(amount, address(this));
+
+        assertGt(wrapped * adapter.price(), (amount * 9900 / BPS));
+        
+        SafeERC20.safeApprove(adapter.token(), address(adapter), wrapped);
+
+        uint256 unwrapped = adapter.unwrap(wrapped, address(0xbeef));
+        
+        assertGt(unwrapped, amount * 9900 / BPS);
+        assertGt(IERC20(adapter.underlyingToken()).balanceOf(address(0xbeef)), amount * 9900 / BPS);
     }
 }
