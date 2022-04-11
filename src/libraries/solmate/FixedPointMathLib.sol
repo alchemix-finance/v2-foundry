5e7a8d468542486b146345c4d0c43d1af4bd8a2f
hesnicewithit <splitthedutch@gmail.com>
2022-04-11 16:25:19 -0600

    Changed solidity versions for libraries

--- src/libraries/solmate/FixedPointMathLib.sol
+++ src/libraries/solmate/FixedPointMathLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+pragma solidity 0.8.13;
 
 /// @notice Arithmetic library with operations for fixed-point numbers.
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/FixedPointMathLib.sol)
