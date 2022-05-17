b18e5e05e6ab22dd93ebd001db56e200c87c0cf6
toyvo <toyvo.mail@proton.me>
2022-05-17 08:23:16 -0500

    all v2 contracts cutover

---
+++ src/AlchemicTokenV2Base.sol
@@ -0,0 +1,259 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.11;
+
+import {AccessControlUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import {ERC20Upgradeable} from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/security/ReentrancyGuardUpgradeable.sol";
+
+import {IllegalArgument, IllegalState, Unauthorized} from "./base/Errors.sol";
+
+import {IERC3156FlashLender} from "openzeppelin-contracts/contracts/interfaces/IERC3156FlashLender.sol";
+import {IERC3156FlashBorrower} from "openzeppelin-contracts/contracts/interfaces/IERC3156FlashBorrower.sol";
+
+/// @title  AlchemicTokenV2
+/// @author Alchemix Finance
+///
+/// @notice This is the contract for version two alchemic tokens.
+/// @notice Initially, the contract deployer is given both the admin and minter role. This allows them to pre-mine
+///         tokens, transfer admin to a timelock contract, and lastly, grant the staking pools the minter role. After
+///         this is done, the deployer must revoke their admin role and minter role.
+contract AlchemicTokenV2Base is ERC20Upgradeable, AccessControlUpgradeable, IERC3156FlashLender, ReentrancyGuardUpgradeable {
+  /// @notice The identifier of the role which maintains other roles.
+  bytes32 public constant ADMIN_ROLE = keccak256("ADMIN");
+
+  /// @notice The identifier of the role which allows accounts to mint tokens.
+  bytes32 public constant SENTINEL_ROLE = keccak256("SENTINEL");
+
+  /// @notice The expected return value from a flash mint receiver
+  bytes32 public constant CALLBACK_SUCCESS = keccak256("ERC3156FlashBorrower.onFlashLoan");
+
+  /// @notice The maximum number of basis points needed to represent 100%.
+  uint256 public constant BPS = 10000;
+
+  /// @notice A set of addresses which are whitelisted for minting new tokens.
+  mapping(address => bool) public whitelisted;
+
+  /// @notice A set of addresses which are paused from minting new tokens.
+  mapping(address => bool) public paused;
+
+  /// @notice The amount that each address is permitted to mint.
+  mapping(address => uint256) public mintCeiling;
+
+  /// @notice The amount of tokens that each address has already minted.
+  mapping(address => uint256) public totalMinted;
+
+  /// @notice Fee for flash minting
+  uint256 public flashMintFee;
+
+  /// @notice Max flash mint amount
+  uint256 public maxFlashLoanAmount;
+
+  /// @notice An event which is emitted when a minter is paused from minting.
+  ///
+  /// @param minter The address of the minter which was paused.
+  /// @param state  A flag indicating if the alchemist is paused or unpaused.
+  event Paused(address minter, bool state);
+
+  /// @notice An event which is emitted when the flash mint fee is updated.
+  ///
+  /// @param fee The new flash mint fee.
+  event SetFlashMintFee(uint256 fee);
+
+  function __AlchemicTokenV2Base_init() internal {
+    _setupRole(ADMIN_ROLE, msg.sender);
+    _setupRole(SENTINEL_ROLE, msg.sender);
+    _setRoleAdmin(SENTINEL_ROLE, ADMIN_ROLE);
+    _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+  }
+
+  /// @dev A modifier which checks that the caller has the admin role.
+  modifier onlyAdmin() {
+    if (!hasRole(ADMIN_ROLE, msg.sender)) {
+      revert Unauthorized("Not admin");
+    }
+    _;
+  }
+
+  /// @dev A modifier which checks that the caller has the sentinel role.
+  modifier onlySentinel() {
+    if(!hasRole(SENTINEL_ROLE, msg.sender)) {
+      revert Unauthorized("Not sentinel");
+    }
+    _;
+  }
+
+  /// @dev A modifier which checks if whitelisted for minting.
+  modifier onlyWhitelisted() {
+    if(!whitelisted[msg.sender]) {
+      revert Unauthorized("Not whitelisted");
+    }
+    _;
+  }
+
+  /// @notice Sets the flash minting fee.
+  ///
+  /// @notice This function reverts if `msg.sender` is not an admin.
+  ///
+  /// @param newFee The new flash mint fee.
+  function setFlashFee(uint256 newFee) external onlyAdmin {
+    flashMintFee = newFee;
+    emit SetFlashMintFee(flashMintFee);
+  }
+
+  /// @notice Mints tokens to `a recipient.`
+  ///
+  /// @notice This function reverts if `msg.sender` is not whitelisted.
+  /// @notice This function reverts if `msg.sender` is paused.
+  /// @notice This function reverts if `msg.sender` has exceeded their mintable ceiling.
+  ///
+  /// @param recipient The address to mint the tokens to.
+  /// @param amount    The amount of tokens to mint.
+  function mint(address recipient, uint256 amount) external onlyWhitelisted {
+    if (paused[msg.sender]) {
+      revert IllegalState("msg.sender is paused");
+    }
+
+    uint256 total = amount + totalMinted[msg.sender];
+    if (total > mintCeiling[msg.sender]) {
+      revert IllegalState("mintable ceiling exceeded");
+    }
+
+    totalMinted[msg.sender] = total;
+
+    _mint(recipient, amount);
+  }
+
+  /// @notice Sets `minter` as whitelisted to mint.
+  ///
+  /// @notice This function reverts if `msg.sender` is not an admin.
+  ///
+  /// @param minter The account to permit to mint.
+  /// @param state  A flag indicating if the minter should be able to mint.
+  function setWhitelist(address minter, bool state) external onlyAdmin {
+    whitelisted[minter] = state;
+  }
+
+  /// @notice Sets `sentinel` as a sentinel.
+  ///
+  /// @notice This function reverts if `msg.sender` is not an admin.
+  ///
+  /// @param sentinel The address to set as a sentinel.
+  function setSentinel(address sentinel) external onlyAdmin {
+    _setupRole(SENTINEL_ROLE, sentinel);
+  }
+
+  /// @notice Sets the maximum amount of tokens that `minter` is allowed to mint.
+  ///
+  /// @notice This function reverts if `msg.sender` is not an admin.
+  ///
+  /// @param minter  The address of the minter.
+  /// @param maximum The maximum amount of tokens that the minter is allowed to mint.
+  function setCeiling(address minter, uint256 maximum) external onlyAdmin {
+    mintCeiling[minter] = maximum;
+  }
+
+  /// @notice Pauses `minter` from minting tokens.
+  ///
+  /// @notice This function reverts if `msg.sender` is not a sentinel.
+  ///
+  /// @param minter The address to set as paused or unpaused.
+  /// @param state  A flag indicating if the minter should be paused or unpaused.
+  function pauseMinter(address minter, bool state) external onlySentinel {
+    paused[minter] = state;
+    emit Paused(minter, state);
+  }
+
+  /// @notice Burns `amount` tokens from `msg.sender`.
+  ///
+  /// @param amount The amount of tokens to be burned.
+  function burn(uint256 amount) external {
+    _burn(msg.sender, amount);
+  }
+
+  /// @dev Destroys `amount` tokens from `account`, deducting from the caller's allowance.
+  ///
+  /// @param account The address the burn tokens from.
+  /// @param amount  The amount of tokens to burn.
+  function burnFrom(address account, uint256 amount) external {
+    uint256 newAllowance = allowance(account, msg.sender) - amount;
+
+    _approve(account, msg.sender, newAllowance);
+    _burn(account, amount);
+  }
+
+  /// @notice Lowers the number of tokens which the `msg.sender` has minted.
+  ///
+  /// @notice This reverts if the `msg.sender` is not whitelisted.
+  ///
+  /// @param amount The amount to lower the minted amount by.
+  function lowerHasMinted(uint256 amount) external onlyWhitelisted {
+    totalMinted[msg.sender] = totalMinted[msg.sender] - amount;
+  }
+
+  /// @notice Adjusts the maximum flashloan amount.
+  ///
+  /// @param _maxFlashLoanAmount The maximum flashloan amount.
+  function setMaxFlashLoan(uint _maxFlashLoanAmount) external onlyAdmin {
+    maxFlashLoanAmount = _maxFlashLoanAmount;
+  }
+
+  /// @notice Gets the maximum amount to be flash loaned of a token.
+  ///
+  /// @param token The address of the token.
+  ///
+  /// @return The maximum amount of `token` that can be flashed loaned.
+  function maxFlashLoan(address token) public view override returns (uint256) {
+    if (token != address(this)) {
+      return 0;
+    }
+    return maxFlashLoanAmount;
+  }
+
+  /// @notice Gets the flash loan fee of `amount` of `token`.
+  ///
+  /// @param token  The address of the token.`
+  /// @param amount The amount of `token` to flash mint.
+  ///
+  /// @return The flash loan fee.
+  function flashFee(address token, uint256 amount) public view override returns (uint256) {
+    if (token != address(this)) {
+      revert IllegalArgument("Illegal token argument");
+    }
+    return amount * flashMintFee / BPS;
+  }
+
+  /// @notice Performs a flash mint (called flash loan to confirm with ERC3156 standard).
+  ///
+  /// @param receiver The address which will receive the flash minted tokens.
+  /// @param token    The address of the token to flash mint.
+  /// @param amount   How much to flash mint.
+  /// @param data     ABI encoded data to pass to the receiver.
+  ///
+  /// @return If the flash loan was successful.
+  function flashLoan(
+    IERC3156FlashBorrower receiver,
+    address token,
+    uint256 amount,
+    bytes calldata data
+  ) external override nonReentrant returns (bool) {
+    if (token != address(this)) {
+      revert IllegalArgument("Illegal token argument");
+    }
+
+    if (amount > maxFlashLoan(token)) {
+      revert IllegalArgument("Amount greater than max flash loan");
+    }
+
+    uint256 fee = flashFee(token, amount);
+
+    _mint(address(receiver), amount);
+
+    if (receiver.onFlashLoan(msg.sender, token, amount, fee, data) != CALLBACK_SUCCESS) {
+      revert IllegalState("Unsuccessful flash loan");
+    }
+
+    _burn(address(receiver), amount + fee); // Will throw error if not enough to burn
+
+    return true;
+  }
+}
