05c73ef0fb0b353865eb9b537405184230f4e5b2
Lucas MT <lucas.tabajara@runtimeverification.com>
2022-05-23 18:27:33 -0500

    Add Whitelist and Test contracts to foundry repo

---
+++ src/test/TestYieldToken.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.8.11;
+
+import { ERC20 } from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+import "../libraries/TokenUtils.sol";
+import "../interfaces/test/ITestYieldToken.sol";
+import "./TestERC20.sol";
+
+/// @title  TestYieldToken
+/// @author Alchemix Finance
+contract TestYieldToken is ITestYieldToken, ERC20 {
+  address private constant BLACKHOLE = address(0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB);
+  uint256 private constant BPS = 10000;
+
+  address public override underlyingToken;
+  uint8 private _decimals;
+  uint256 public slippage;
+
+  constructor(address _underlyingToken) ERC20("Yield Token", "Yield Token") {
+    underlyingToken = _underlyingToken;
+    _decimals = TokenUtils.expectDecimals(_underlyingToken);
+    slippage = 0;
+  }
+
+  function decimals() public view override returns (uint8) {
+    return _decimals;
+  }
+
+  function price() external view override returns (uint256) {
+    return _shareValue(10**_decimals);
+  }
+
+  function setSlippage(uint256 _slippage) external {
+    slippage = _slippage;
+  }
+
+  function mint(uint256 amount, address recipient) external override returns (uint256) {
+    assert(amount > 0);
+
+    uint256 shares = _issueSharesForAmount(recipient, amount);
+
+    TokenUtils.safeTransferFrom(underlyingToken, msg.sender, address(this), amount);
+
+    return shares;
+  }
+
+  function redeem(uint256 shares, address recipient) external override returns (uint256) {
+    assert(shares > 0);
+
+    uint256 value = _shareValue(shares);
+    value = (value * (BPS - slippage)) / BPS;
+    _burn(msg.sender, shares);
+    TokenUtils.safeTransfer(underlyingToken, recipient, value);
+
+    return value;
+  }
+
+  function slurp(uint256 amount) external override {
+    TokenUtils.safeTransferFrom(underlyingToken, msg.sender, address(this), amount);
+  }
+
+  function siphon(uint256 amount) external override {
+    TokenUtils.safeTransfer(underlyingToken, BLACKHOLE, amount);
+  }
+
+  function _issueSharesForAmount(address to, uint256 amount) internal returns (uint256) {
+    uint256 shares = 0;
+    if (totalSupply() > 0) {
+      shares = (amount * totalSupply()) / TokenUtils.safeBalanceOf(underlyingToken, address(this));
+    } else {
+      shares = amount;
+    }
+    shares = (shares * (BPS - slippage)) / BPS;
+    _mint(to, shares);
+    return shares;
+  }
+
+  function _shareValue(uint256 shares) internal view returns (uint256) {
+    if (totalSupply() == 0) {
+      return shares;
+    }
+    return (shares * TokenUtils.safeBalanceOf(underlyingToken, address(this))) / totalSupply();
+  }
+}
---
+++ src/test/TestYieldTokenAdapter.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.11;
+
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+import "../interfaces/ITokenAdapter.sol";
+import "../interfaces/test/ITestYieldToken.sol";
+
+import "../libraries/TokenUtils.sol";
+
+/// @title  TestYieldTokenAdapter
+/// @author Alchemix Finance
+contract TestYieldTokenAdapter is ITokenAdapter {
+    string public constant override version = "2.0.0";
+    
+    /// @inheritdoc ITokenAdapter
+    address public immutable override token;
+
+    /// @inheritdoc ITokenAdapter
+    address public immutable override underlyingToken;
+
+    constructor(address _token) {
+        token = _token;
+        underlyingToken = ITestYieldToken(_token).underlyingToken();
+        IERC20(ITestYieldToken(_token).underlyingToken()).approve(_token, type(uint256).max);
+    }
+
+    /// @inheritdoc ITokenAdapter
+    function price() external view override returns (uint256) {
+        return ITestYieldToken(token).price();
+    }
+
+    /// @inheritdoc ITokenAdapter
+    function wrap(uint256 amount, address recipient) external override returns (uint256) {
+        TokenUtils.safeTransferFrom(underlyingToken, msg.sender, address(this), amount);
+        return ITestYieldToken(token).mint(amount, recipient);
+    }
+
+    /// @inheritdoc ITokenAdapter
+    function unwrap(uint256 amount, address recipient) external override returns (uint256) {
+        TokenUtils.safeTransferFrom(token, msg.sender, address(this), amount);
+        return ITestYieldToken(token).redeem(amount, recipient);
+    }
+}
