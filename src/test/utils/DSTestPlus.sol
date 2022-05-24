b23fe2d61c0e35b06ffed9931e1b88fc16d2864b
thetechnocratic <thetechnocratic@gmail.com>
2022-05-23 23:40:46 -0400

    formatting.

--- src/test/utils/DSTestPlus.sol
+++ src/test/utils/DSTestPlus.sol
@@ -8,159 +8,159 @@ import { Hevm } from "./Hevm.sol";
 /// @notice Extended testing framework for DappTools projects.
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/test/utils/DSTestPlus.sol)
 contract DSTestPlus is DSTest {
-  Hevm internal constant hevm = Hevm(HEVM_ADDRESS);
+	Hevm internal constant hevm = Hevm(HEVM_ADDRESS);
 
-  address internal constant DEAD_ADDRESS = 0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF;
+	address internal constant DEAD_ADDRESS = 0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF;
 
-  string private checkpointLabel;
-  uint256 private checkpointGasLeft;
+	string private checkpointLabel;
+	uint256 private checkpointGasLeft;
 
-  function startMeasuringGas(string memory label) internal virtual {
-    checkpointLabel = label;
-    checkpointGasLeft = gasleft();
-  }
+	function startMeasuringGas(string memory label) internal virtual {
+		checkpointLabel = label;
+		checkpointGasLeft = gasleft();
+	}
 
-  function stopMeasuringGas() internal virtual {
-    uint256 checkpointGasLeft2 = gasleft();
+	function stopMeasuringGas() internal virtual {
+		uint256 checkpointGasLeft2 = gasleft();
 
-    string memory label = checkpointLabel;
+		string memory label = checkpointLabel;
 
-    emit log_named_uint(string(abi.encodePacked(label, " Gas")), checkpointGasLeft - checkpointGasLeft2);
-  }
+		emit log_named_uint(string(abi.encodePacked(label, " Gas")), checkpointGasLeft - checkpointGasLeft2);
+	}
 
-  function fail(string memory err) internal virtual {
-    emit log_named_string("Error", err);
-    fail();
-  }
+	function fail(string memory err) internal virtual {
+		emit log_named_string("Error", err);
+		fail();
+	}
 
-  function assertFalse(bool data) internal virtual {
-    assertTrue(!data);
-  }
+	function assertFalse(bool data) internal virtual {
+		assertTrue(!data);
+	}
 
-  function assertUint128Eq(uint128 a, uint128 b) internal virtual {
-    assertEq(uint256(a), uint256(b));
-  }
+	function assertUint128Eq(uint128 a, uint128 b) internal virtual {
+		assertEq(uint256(a), uint256(b));
+	}
 
-  function assertUint64Eq(uint64 a, uint64 b) internal virtual {
-    assertEq(uint256(a), uint256(b));
-  }
+	function assertUint64Eq(uint64 a, uint64 b) internal virtual {
+		assertEq(uint256(a), uint256(b));
+	}
 
-  function assertUint96Eq(uint96 a, uint96 b) internal virtual {
-    assertEq(uint256(a), uint256(b));
-  }
+	function assertUint96Eq(uint96 a, uint96 b) internal virtual {
+		assertEq(uint256(a), uint256(b));
+	}
 
-  function assertUint32Eq(uint32 a, uint32 b) internal virtual {
-    assertEq(uint256(a), uint256(b));
-  }
+	function assertUint32Eq(uint32 a, uint32 b) internal virtual {
+		assertEq(uint256(a), uint256(b));
+	}
 
-  function assertBoolEq(bool a, bool b) internal virtual {
-    b ? assertTrue(a) : assertFalse(a);
-  }
+	function assertBoolEq(bool a, bool b) internal virtual {
+		b ? assertTrue(a) : assertFalse(a);
+	}
 
-  function assertApproxEq(
-    uint256 a,
-    uint256 b,
-    uint256 maxDelta
-  ) internal virtual {
-    uint256 delta = a > b ? a - b : b - a;
+	function assertApproxEq(
+		uint256 a,
+		uint256 b,
+		uint256 maxDelta
+	) internal virtual {
+		uint256 delta = a > b ? a - b : b - a;
 
-    if (delta > maxDelta) {
-      emit log("Error: a ~= b not satisfied [uint]");
-      emit log_named_uint("  Expected", a);
-      emit log_named_uint("    Actual", b);
-      emit log_named_uint(" Max Delta", maxDelta);
-      emit log_named_uint("     Delta", delta);
-      fail();
-    }
-  }
+		if (delta > maxDelta) {
+			emit log("Error: a ~= b not satisfied [uint]");
+			emit log_named_uint("  Expected", a);
+			emit log_named_uint("    Actual", b);
+			emit log_named_uint(" Max Delta", maxDelta);
+			emit log_named_uint("     Delta", delta);
+			fail();
+		}
+	}
 
-  function assertRelApproxEq(
-    uint256 a,
-    uint256 b,
-    uint256 maxPercentDelta
-  ) internal virtual {
-    uint256 delta = a > b ? a - b : b - a;
-    uint256 abs = a > b ? a : b;
+	function assertRelApproxEq(
+		uint256 a,
+		uint256 b,
+		uint256 maxPercentDelta
+	) internal virtual {
+		uint256 delta = a > b ? a - b : b - a;
+		uint256 abs = a > b ? a : b;
 
-    uint256 percentDelta = (delta * 1e18) / abs;
+		uint256 percentDelta = (delta * 1e18) / abs;
 
-    if (percentDelta > maxPercentDelta) {
-      emit log("Error: a ~= b not satisfied [uint]");
-      emit log_named_uint("    Expected", a);
-      emit log_named_uint("      Actual", b);
-      emit log_named_uint(" Max % Delta", maxPercentDelta);
-      emit log_named_uint("     % Delta", percentDelta);
-      fail();
-    }
-  }
+		if (percentDelta > maxPercentDelta) {
+			emit log("Error: a ~= b not satisfied [uint]");
+			emit log_named_uint("    Expected", a);
+			emit log_named_uint("      Actual", b);
+			emit log_named_uint(" Max % Delta", maxPercentDelta);
+			emit log_named_uint("     % Delta", percentDelta);
+			fail();
+		}
+	}
 
-  function assertBytesEq(bytes memory a, bytes memory b) internal virtual {
-    if (keccak256(a) != keccak256(b)) {
-      emit log("Error: a == b not satisfied [bytes]");
-      emit log_named_bytes("  Expected", b);
-      emit log_named_bytes("    Actual", a);
-      fail();
-    }
-  }
+	function assertBytesEq(bytes memory a, bytes memory b) internal virtual {
+		if (keccak256(a) != keccak256(b)) {
+			emit log("Error: a == b not satisfied [bytes]");
+			emit log_named_bytes("  Expected", b);
+			emit log_named_bytes("    Actual", a);
+			fail();
+		}
+	}
 
-  function assertUintArrayEq(uint256[] memory a, uint256[] memory b) internal virtual {
-    require(a.length == b.length, "LENGTH_MISMATCH");
+	function assertUintArrayEq(uint256[] memory a, uint256[] memory b) internal virtual {
+		require(a.length == b.length, "LENGTH_MISMATCH");
 
-    for (uint256 i = 0; i < a.length; i++) {
-      assertEq(a[i], b[i]);
-    }
-  }
+		for (uint256 i = 0; i < a.length; i++) {
+			assertEq(a[i], b[i]);
+		}
+	}
 
-  function expectError(string memory message) internal {
-    hevm.expectRevert(bytes(message));
-  }
+	function expectError(string memory message) internal {
+		hevm.expectRevert(bytes(message));
+	}
 
-  function expectIllegalArgumentError(string memory message) internal {
-    hevm.expectRevert(abi.encodeWithSignature("IllegalArgument(string)", message));
-  }
+	function expectIllegalArgumentError(string memory message) internal {
+		hevm.expectRevert(abi.encodeWithSignature("IllegalArgument(string)", message));
+	}
 
-  function expectIllegalStateError(string memory message) internal {
-    hevm.expectRevert(abi.encodeWithSignature("IllegalState(string)", message));
-  }
+	function expectIllegalStateError(string memory message) internal {
+		hevm.expectRevert(abi.encodeWithSignature("IllegalState(string)", message));
+	}
 
-  function expectUnauthorizedError(string memory message) internal {
-    hevm.expectRevert(abi.encodeWithSignature("Unauthorized(string)", message));
-  }
+	function expectUnauthorizedError(string memory message) internal {
+		hevm.expectRevert(abi.encodeWithSignature("Unauthorized(string)", message));
+	}
 
-  function expectUnsupportedOperationError(string memory message) internal {
-    hevm.expectRevert(abi.encodeWithSignature("UnsupportedOperation(string)", message));
-  }
+	function expectUnsupportedOperationError(string memory message) internal {
+		hevm.expectRevert(abi.encodeWithSignature("UnsupportedOperation(string)", message));
+	}
 
-  function bound(
-    uint256 x,
-    uint256 min,
-    uint256 max
-  ) internal pure returns (uint256 result) {
-    require(max >= min, "MAX_LESS_THAN_MIN");
+	function bound(
+		uint256 x,
+		uint256 min,
+		uint256 max
+	) internal pure returns (uint256 result) {
+		require(max >= min, "MAX_LESS_THAN_MIN");
 
-    uint256 size = max - min;
+		uint256 size = max - min;
 
-    if (max != type(uint256).max) size++; // Make the max inclusive.
-    if (size == 0) return min; // Using max would be equivalent as well.
-    // Ensure max is inclusive in cases where x != 0 and max is at uint max.
-    if (max == type(uint256).max && x != 0) x--; // Accounted for later.
+		if (max != type(uint256).max) size++; // Make the max inclusive.
+		if (size == 0) return min; // Using max would be equivalent as well.
+		// Ensure max is inclusive in cases where x != 0 and max is at uint max.
+		if (max == type(uint256).max && x != 0) x--; // Accounted for later.
 
-    if (x < min) x += size * (((min - x) / size) + 1);
-    result = min + ((x - min) % size);
+		if (x < min) x += size * (((min - x) / size) + 1);
+		result = min + ((x - min) % size);
 
-    // Account for decrementing x to make max inclusive.
-    if (max == type(uint256).max && x != 0) result++;
-  }
+		// Account for decrementing x to make max inclusive.
+		if (max == type(uint256).max && x != 0) result++;
+	}
 
-  function min3(
-    uint256 a,
-    uint256 b,
-    uint256 c
-  ) internal pure returns (uint256) {
-    return a > b ? (b > c ? c : b) : (a > c ? c : a);
-  }
+	function min3(
+		uint256 a,
+		uint256 b,
+		uint256 c
+	) internal pure returns (uint256) {
+		return a > b ? (b > c ? c : b) : (a > c ? c : a);
+	}
 
-  function min2(uint256 a, uint256 b) internal pure returns (uint256) {
-    return a > b ? b : a;
-  }
+	function min2(uint256 a, uint256 b) internal pure returns (uint256) {
+		return a > b ? b : a;
+	}
 }
