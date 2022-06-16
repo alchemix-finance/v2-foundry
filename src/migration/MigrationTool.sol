9cc91b2f072b833d1508ebc161bb7ab1ff534eca
hesnicewithit <splitthedutch@gmail.com>
2022-06-16 02:16:41 -0600

    Code cleanup and consolidation. Pending math fix for ETH migrator

--- src/migration/MigrationTool.sol
+++ src/migration/MigrationTool.sol
@@ -17,7 +17,6 @@ import {SafeERC20} from "../libraries/SafeERC20.sol";
 import {IAlToken} from "../interfaces/IAlToken.sol";
 import {IAlchemistV2} from "../interfaces/IAlchemistV2.sol";
 import {IAlchemistV2State} from "../interfaces/alchemist/IAlchemistV2State.sol";
-import {ICurveMetapool} from "../interfaces/ICurveMetapool.sol";
 import {IMigrationTool} from "../interfaces/IMigrationTool.sol";
 import {IStableSwap3Pool} from "../interfaces/external/curve/IStableSwap3Pool.sol";
 import {IWETH9} from "../interfaces/external/IWETH9.sol";
@@ -28,25 +27,31 @@ struct InitializationParams {
     address curveThreePool;
 }
 
-contract MigrationToolUSD is IMigrationTool, Multicall {
+struct UnderlyingToken {
+    int128 index;
+    uint256 decimals;
+}
+
+contract MigrationTool is IMigrationTool, Multicall {
     string public override version = "1.0.0";
 
-    mapping(address => int128) public indexes;
+    mapping(address => UnderlyingToken) public underlyingTokens;
 
     IAlchemistV2 public immutable Alchemist;
     IAlToken public immutable AlchemicToken;
-    ICurveMetapool public immutable CurveMetapool;
     IStableSwap3Pool public immutable CurveThreePool;
 
     constructor(InitializationParams memory params) {
         Alchemist       = IAlchemistV2(params.alchemist);
         AlchemicToken   = IAlToken(Alchemist.debtToken());
-        CurveMetapool   = ICurveMetapool(params.curveMetapool);
         CurveThreePool  = IStableSwap3Pool(params.curveThreePool);
 
-        indexes[0x6B175474E89094C44Da98b954EedeAC495271d0F] = 0;
-        indexes[0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48] = 1;
-        indexes[0xdAC17F958D2ee523a2206206994597C13D831ec7] = 2;
+        // Addresses for underlying tokens if user swaps between collateral
+        // Only for stable coins
+        underlyingTokens[0x6B175474E89094C44Da98b954EedeAC495271d0F] = UnderlyingToken(0, 18);
+        underlyingTokens[0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48] = UnderlyingToken(1, 6);
+        underlyingTokens[0xdAC17F958D2ee523a2206206994597C13D831ec7] = UnderlyingToken(2, 6);
+        underlyingTokens[0xa258C4606Ca8206D8aA700cE2143D7db854D168c] = UnderlyingToken(3, 18);
     }
 
     /// @inheritdoc IMigrationTool
@@ -56,9 +61,6 @@ contract MigrationToolUSD is IMigrationTool, Multicall {
         uint256 shares,
         uint256 minReturn
     ) external override payable returns(uint256) {
-        IAlchemistV2State.YieldTokenParams memory startingParams = Alchemist.getYieldTokenParameters(startingVault);
-        IAlchemistV2State.YieldTokenParams memory targetParams = Alchemist.getYieldTokenParameters(targetVault);
-
         // If either vault is invalid, revert
         if(!Alchemist.isSupportedYieldToken(startingVault)) {
             revert IllegalArgument("Vault is not supported");
@@ -68,6 +70,17 @@ contract MigrationToolUSD is IMigrationTool, Multicall {
             revert IllegalArgument("Vault is not supported");
         }
 
+        // Vaults cannot be the same due prevent slippage on current position
+        if(startingVault == targetVault) {
+            revert IllegalArgument("Vaults cannot be the same");
+        }
+
+        IAlchemistV2State.YieldTokenParams memory startingParams = Alchemist.getYieldTokenParameters(startingVault);
+        IAlchemistV2State.YieldTokenParams memory targetParams = Alchemist.getYieldTokenParameters(targetVault);
+
+        // Conversion from shares
+        uint256 debtTokenValue = (shares * Alchemist.getUnderlyingTokensPerShare(startingVault) / 10**underlyingTokens[startingParams.underlyingToken].decimals) * 10**(18 - underlyingTokens[startingParams.underlyingToken].decimals);
+
         (int256 debt, ) = Alchemist.accounts(msg.sender);
 
         // Debt must be positive, otherwise this tool is not needed to withdraw and re-deposit
@@ -75,37 +88,35 @@ contract MigrationToolUSD is IMigrationTool, Multicall {
             revert IllegalState("Debt must be positive");
         }
 
-        AlchemicToken.mint(address(this), shares / 2);
-
-        SafeERC20.safeApprove(Alchemist.debtToken(), address(CurveMetapool), shares / 2);
-        // TODO change the second param to be a enum
-        uint256 exchanged = CurveMetapool.exchange_underlying(0, indexes[startingParams.underlyingToken] + 1, shares / 2, 0);
-
-        // Repay with underlying received from exchange
-        SafeERC20.safeApprove(startingParams.underlyingToken, address(Alchemist), exchanged);
-        Alchemist.repay(startingParams.underlyingToken, exchanged, msg.sender);
+        AlchemicToken.mint(address(this), debtTokenValue / 2);
+        SafeERC20.safeApprove(address(AlchemicToken), address(Alchemist), debtTokenValue / 2);
+        Alchemist.burn(debtTokenValue / 2, msg.sender);
 
         // Withdraw what you can from the old position
         // TODO figure out how to withdraw as much as possible
-        uint256 underlyingReturned = Alchemist.withdrawUnderlyingFrom(msg.sender, startingVault, shares * 9950 / 10000, address(this), 0);
+        // TODO find better variable names
+        uint256 underlyingReturned = Alchemist.withdrawUnderlyingFrom(msg.sender, startingVault, debtTokenValue * 9700 / 10000, address(this), minReturn);
 
         // If starting and target underlying tokens are not the same then make 3pool swap
-        // TODO make enum
-        // TODO update minimums
         if(startingParams.underlyingToken != targetParams.underlyingToken) {
             SafeERC20.safeApprove(startingParams.underlyingToken, address(CurveThreePool), underlyingReturned);
-            CurveThreePool.exchange(indexes[startingParams.underlyingToken], indexes[targetParams.underlyingToken], underlyingReturned, 0);
+            CurveThreePool.exchange(underlyingTokens[startingParams.underlyingToken].index, underlyingTokens[targetParams.underlyingToken].index, underlyingReturned, minReturn);
             underlyingReturned = IERC20(targetParams.underlyingToken).balanceOf(address(this));
         }
 
         // Deposit into new vault
         SafeERC20.safeApprove(targetParams.underlyingToken, address(Alchemist), underlyingReturned);
-        uint256 sharesReturned = Alchemist.depositUnderlying(targetVault, underlyingReturned, msg.sender, 0);
+        uint256 sharesReturned = Alchemist.depositUnderlying(targetVault, underlyingReturned, msg.sender, minReturn);
+        uint256 underlyingValueReturned = (sharesReturned * Alchemist.getUnderlyingTokensPerShare(startingVault) / 10**underlyingTokens[targetParams.underlyingToken].decimals) * 10**(18 - underlyingTokens[targetParams.underlyingToken].decimals);
 
-        // mint al token which will be burned to fulfill flash loan requirements
+        // Mint al token which will be burned to fulfill flash loan requirements
         Alchemist.mintFrom(msg.sender, (sharesReturned/2), address(this));
         AlchemicToken.burn(sharesReturned/2);
 
 		return sharesReturned;
 	}
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
 }
---
+++ src/test/MigrationTool.t.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.13;
+
+//remove later
+import {console} from "forge-std/console.sol";
+
+import {IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+import {DSTestPlus} from "./utils/DSTestPlus.sol";
+import {stdCheats} from "forge-std/stdlib.sol";
+
+import {
+    MigrationTool,
+    InitializationParams as MigrtionInitializationParams
+} from "../migration/MigrationTool.sol";
+
+import {SafeERC20} from "../libraries/SafeERC20.sol";
+
+import {IAlToken} from "../interfaces/IAlToken.sol";
+import {IAlchemistV2} from "../interfaces/IAlchemistV2.sol";
+import {IWhitelist} from "../interfaces/IWhitelist.sol";
+
+import {ICurveMetapool} from "../interfaces/ICurveMetapool.sol";
+
+
+contract MigrationToolTest is DSTestPlus, stdCheats {
+    address constant admin = 0x8392F6669292fA56123F71949B52d883aE57e225;
+    address constant alchemistETH = 0x062Bf725dC4cDF947aa79Ca2aaCCD4F385b13b5c;
+    address constant alchemistUSD = 0x5C6374a2ac4EBC38DeA0Fc1F8716e5Ea1AdD94dd;
+    address constant alETH = 0x0100546F2cD4C9D97f798fFC9755E47865FF7Ee6;
+    address constant alUSD = 0xBC6DA0FE9aD5f3b0d58160288917AA56653660E9;
+    address constant curveMetapool = 0x43b4FdFD4Ff969587185cDB6f0BD875c5Fc83f8c;
+    address constant curveThreePool = 0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7;
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant invalidYieldToken = 0x23D3D0f1c697247d5e0a9efB37d8b0ED0C464f7f;
+    address constant owner = 0x9e2b6378ee8ad2A4A95Fe481d63CAba8FB0EBBF9;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant rETH = 0xae78736Cd615f374D3085123A210448E74Fc6393;
+    address constant wETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant whitelistETH = 0xA3dfCcbad1333DC69997Da28C961FF8B2879e653;
+    address constant whitelistUSD = 0x78537a6CeBa16f412E123a90472C6E0e9A8F1132;
+    address constant wstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address constant yvDAI = 0xdA816459F1AB5631232FE5e97a05BBBb94970c95;
+    address constant yvETH = 0xa258C4606Ca8206D8aA700cE2143D7db854D168c;
+    address constant yvUSDC = 0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE;
+    uint256 constant BPS = 10000;
+    uint256 constant MAX_INT = 2**256 - 1;
+
+
+    IAlToken AlUSD;
+    IAlToken AlETH;
+    IAlchemistV2 AlchemistUSD;
+    IAlchemistV2 AlchemistETH;
+    IWhitelist WhitelistUSD;
+    IWhitelist WhitelistETH;
+
+    MigrationTool migrationToolETH;
+    MigrationTool migrationToolUSD;
+
+    function setUp() external {
+        migrationToolETH = new MigrationTool(MigrtionInitializationParams({
+            alchemist:       alchemistETH,
+            curveMetapool:  curveMetapool,
+            curveThreePool:  curveThreePool
+        }));
+
+        migrationToolUSD = new MigrationTool(MigrtionInitializationParams({
+            alchemist:       alchemistUSD,
+            curveMetapool:  curveMetapool,
+            curveThreePool:  curveThreePool
+        }));
+
+        AlUSD = IAlToken(alUSD);
+        AlETH = IAlToken(alETH);
+
+        AlchemistUSD = IAlchemistV2(alchemistUSD);
+        AlchemistETH = IAlchemistV2(alchemistETH);
+
+        WhitelistUSD = IWhitelist(whitelistUSD);
+        WhitelistETH = IWhitelist(whitelistETH);
+
+        hevm.startPrank(admin);
+        AlETH.setWhitelist(address(migrationToolETH), true);
+        AlETH.setCeiling(address(migrationToolETH), MAX_INT);
+        AlUSD.setWhitelist(address(migrationToolUSD), true);
+        AlUSD.setCeiling(address(migrationToolUSD), MAX_INT);
+        hevm.stopPrank();
+
+        hevm.startPrank(owner);
+        WhitelistETH.add(address(this));
+        WhitelistETH.add(address(0xbeef));
+        WhitelistETH.add(address(migrationToolETH));
+        WhitelistUSD.add(address(this));
+        WhitelistUSD.add(address(0xbeef));
+        WhitelistUSD.add(address(migrationToolUSD));
+        hevm.stopPrank();
+    }
+
+    function testUnsupportedVaults() external {
+        expectIllegalArgumentError("Vault is not supported");
+        migrationToolUSD.migrateVaults(invalidYieldToken, yvDAI, 100e18, 99e18);
+        
+        expectIllegalArgumentError("Vault is not supported");
+        migrationToolUSD.migrateVaults(yvDAI , invalidYieldToken, 100e18, 99e18);
+
+        expectIllegalArgumentError("Vault is not supported");
+        migrationToolETH.migrateVaults(invalidYieldToken, rETH, 100e18, 90e18);
+        
+        expectIllegalArgumentError("Vault is not supported");
+        migrationToolETH.migrateVaults(rETH , invalidYieldToken, 100e18, 90e18);
+    }
+
+    function testMigrationSameVault() external {
+        expectIllegalArgumentError("Vaults cannot be the same");
+        migrationToolUSD.migrateVaults(yvDAI, yvDAI, 100e18, 99e18);
+
+        expectIllegalArgumentError("Vaults cannot be the same");
+        migrationToolETH.migrateVaults(yvETH, yvETH, 100e18, 90e18);
+    }
+
+    function testMigrationDifferentUnderlying() external {
+        tip(DAI, address(this), 200e18);
+        
+        // Create new position
+        SafeERC20.safeApprove(DAI, alchemistUSD, 100e18);
+        AlchemistUSD.depositUnderlying(yvDAI, 100e18, address(this), 0);
+
+        (uint256 shares, ) = AlchemistUSD.positions(address(this), yvDAI);
+        AlchemistUSD.mint(shares/2, address(this));
+
+        // Approve the migration tool to withdraw and mint on behalf of the user
+        AlchemistUSD.approveWithdraw(address(migrationToolUSD), yvDAI, shares);
+        AlchemistUSD.approveMint(address(migrationToolUSD), shares);
+
+        uint256 newShares = migrationToolUSD.migrateVaults(yvDAI, yvUSDC, shares, 0);
+        assertGt(newShares * 1e12, shares * 9900 / BPS);
+
+        // TODO add assert to see if alchemist has position for new currency
+    }
+
+    function testMigrationDifferentVault() external {
+        tip(wETH, address(this), 200e18);
+        
+        // Create new position
+        SafeERC20.safeApprove(wETH, alchemistETH, 100e18);
+        AlchemistETH.depositUnderlying(yvETH, 100e18, address(this), 0);
+
+        (uint256 shares, ) = AlchemistETH.positions(address(this), yvETH);
+        AlchemistETH.mint(shares/2, address(this));
+
+        // Approve the migration tool to withdraw and mint on behalf of the user
+        AlchemistETH.approveWithdraw(address(migrationToolETH), yvETH, shares);
+        AlchemistETH.approveMint(address(migrationToolETH), shares);
+
+        uint256 newShares = migrationToolETH.migrateVaults(yvETH, wstETH, shares, 0);
+        // TODO this seems too large a tolerance but the returns are not as good as usd vaults
+        assertGt(newShares, shares * 9000 / BPS);
+
+        // TODO add assert to see if alchemist has position for new currency
+    }
+}
