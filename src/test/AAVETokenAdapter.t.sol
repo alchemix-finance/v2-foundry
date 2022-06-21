034ccb274d33bdee636431f79dc4dde078bcd9b3
thetechnocratic <thetechnocratic@gmail.com>
2022-06-21 10:16:39 -0400

    use better decimal amounts in all vault tests.

--- src/test/AAVETokenAdapter.t.sol
+++ src/test/AAVETokenAdapter.t.sol
@@ -55,13 +55,13 @@ contract AAVETokenAdapterTest is DSTestPlus, stdCheats {
     }
 
     function testAllTokens() external {
-        runTokenTest(alchemistAlUSD, 0x028171bCA77440897B824Ca71D1c56caC55b68A3, 0x6B175474E89094C44Da98b954EedeAC495271d0F, "Static Aave DAI", "saDAI");
-        runTokenTest(alchemistAlUSD, 0xBcca60bB61934080951369a648Fb03DF4F96263C, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, "Static Aave USDC", "saUSDC");
-        runTokenTest(alchemistAlUSD, 0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811, 0xdAC17F958D2ee523a2206206994597C13D831ec7, "Static Aave USDT", "saUSDT");
-        runTokenTest(alchemistAlETH, 0x030bA81f1c18d280636F32af80b9AAd02Cf0854e, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, "Static Aave WETH", "saWETH");
+        runTokenTest(alchemistAlUSD, 0x028171bCA77440897B824Ca71D1c56caC55b68A3, 0x6B175474E89094C44Da98b954EedeAC495271d0F, "Static Aave DAI", "saDAI", 1000 ether);
+        runTokenTest(alchemistAlUSD, 0xBcca60bB61934080951369a648Fb03DF4F96263C, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, "Static Aave USDC", "saUSDC", 1000000000);
+        runTokenTest(alchemistAlUSD, 0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811, 0xdAC17F958D2ee523a2206206994597C13D831ec7, "Static Aave USDT", "saUSDT", 1000000000);
+        runTokenTest(alchemistAlETH, 0x030bA81f1c18d280636F32af80b9AAd02Cf0854e, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, "Static Aave WETH", "saWETH", 1000 ether);
     }
 
-    function runTokenTest(address alchemist, address aToken, address underlyingToken, string memory name, string memory symbol) internal {
+    function runTokenTest(address alchemist, address aToken, address underlyingToken, string memory name, string memory symbol, uint256 amount) internal {
         StaticAToken newStaticAToken = new StaticAToken(
             lendingPool,
             aToken,
@@ -84,7 +84,6 @@ contract AAVETokenAdapterTest is DSTestPlus, stdCheats {
         IAlchemistV2(alchemist).setYieldTokenEnabled(address(newStaticAToken), true);
         hevm.stopPrank();
 
-        uint256 amount = 1000 ether;
         tip(underlyingToken, address(this), amount);
         uint256 startPrice = IAlchemistV2(alchemist).getUnderlyingTokensPerShare(address(newStaticAToken));
         TokenUtils.safeApprove(underlyingToken, alchemist, amount);
