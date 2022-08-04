2b563979a1cbd451a049ab429b7679c351893484
thetechnocratic <thetechnocratic@gmail.com>
2022-08-04 18:20:50 -0400

    add comment.

--- src/migration/V1Deprecatooooor.sol
+++ src/migration/V1Deprecatooooor.sol
@@ -10,6 +10,9 @@ contract V1Deprecatooooor is Ownable {
     function deprecate(address alchemist, address altoken) external onlyOwner {
         IAlToken(altoken).pauseAlchemist(address(alchemist), true);
         uint256 numVaults = IAlchemistV1(alchemist).vaultCount();
+        // we are going to recall the funds from the most recent vault.
+        // we are assuming that the new TransferAdapter has already been deployed and
+        // added to the Alchemist via the migrate() function, which is why we need the (-2).
         IAlchemistV1(alchemist).recallAll(numVaults - 2);
         IAlchemistV1(alchemist).flush();
         IAlchemistV1(alchemist).setEmergencyExit(true);
