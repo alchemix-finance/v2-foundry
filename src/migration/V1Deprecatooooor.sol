7b3cc8d2181ed183de2083b83dedea6b074f1c94
thetechnocratic <thetechnocratic@gmail.com>
2022-08-05 13:45:51 -0400

    pause the transmuter.

--- src/migration/V1Deprecatooooor.sol
+++ src/migration/V1Deprecatooooor.sol
@@ -3,12 +3,14 @@ pragma solidity 0.8.13;
 import {Ownable} from "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IAlchemistV1} from "../interfaces/IAlchemistV1.sol";
 import {IAlToken} from "../interfaces/IAlToken.sol";
+import {ITransmuterV1} from "../interfaces/ITransmuterV1.sol";
 
 contract V1Deprecatooooor is Ownable {
     constructor() {}
 
-    function deprecate(address alchemist, address altoken) external onlyOwner {
+    function deprecate(address alchemist, address altoken, address transmuter) external onlyOwner {
         IAlToken(altoken).pauseAlchemist(address(alchemist), true);
+        ITransmuterV1(transmuter).setPause(true);
         uint256 numVaults = IAlchemistV1(alchemist).vaultCount();
         // we are going to recall the funds from the most recent vault.
         // we are assuming that the new TransferAdapter has already been deployed and
