05058fd6f918eebd08415455d8adf7eb4fb725ac
hesnicewithit <splitthedutch@gmail.com>
2022-08-08 11:21:43 -0600

    Changed import in fuse lib

--- src/libraries/LibFuse.sol
+++ src/libraries/LibFuse.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.13;
 
-import {FixedPointMathLib} from "../../lib/solmate/FixedPointMathLib.sol";
+import {FixedPointMathLib} from "../../lib/solmate/src/utils/FixedPointMathLib.sol";
 
 import {ICERC20} from "../interfaces/external/compound/ICERC20.sol";
 import {IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
