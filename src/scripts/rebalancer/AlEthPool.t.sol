53aa2cf64b4ce4e5aa8edcc7fa6fcb0b13aeaa8f
hesnicewithit <splitthedutch@gmail.com>
2022-08-08 11:52:40 -0600

    Changed remapping to include quotes

--- src/scripts/rebalancer/AlEthPool.t.sol
+++ src/scripts/rebalancer/AlEthPool.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.13;
 
-import { console } from "../../../lib/forge-std/src/console.sol";
 import { IERC20 } from "../../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 import { DSTestPlus } from "../../test/utils/DSTestPlus.sol";
