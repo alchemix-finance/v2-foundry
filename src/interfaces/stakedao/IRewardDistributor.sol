d641a22e6e5ba6025368eb403efe0fba0439baff
hesnicewithit <splitthedutch@gmail.com>
2022-10-07 04:17:02 -0600

    Fixed interface

--- src/interfaces/stakedao/IRewardDistributor.sol
+++ src/interfaces/stakedao/IRewardDistributor.sol
@@ -2,6 +2,4 @@ pragma solidity ^0.8.11;
 
 interface IRewardDistributor {
   function claim() external returns (uint256);
-
-  function vote_for_gauge_weights(address gaugeAddress, uint256 weight) external;
 }
