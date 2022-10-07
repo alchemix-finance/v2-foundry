7e51c7d56efc8390c0dd986ad39738ee3b0342d7
hesnicewithit <splitthedutch@gmail.com>
2022-10-07 03:54:24 -0600

    Added veSDL controlls

--- src/SDLController.sol
+++ src/SDLController.sol
@@ -25,7 +25,6 @@ contract SDLController is Initializable, OwnableUpgradeable {
   function initialize() external initializer {
     __Ownable_init();
     delegateRegistry = 0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446;
-    rewardDistributor = 0x29f3dd38dB24d3935CF1bf841e6b2B461A3E5D92;
     rewardToken = 0x3cF7b9479a01eeB3bbfC43581fa3bb21cd888e2A;
   }
 
@@ -65,14 +64,5 @@ contract SDLController is Initializable, OwnableUpgradeable {
 
   function setRewardToken(address _rewardToken) external onlyOwner {
     rewardToken = _rewardToken;
-  }
-
-  function claim() external onlyOwner {
-    uint256 amountClaimed = IRewardDistributor(rewardDistributor).claim();
-    IERC20Upgradeable(rewardToken).safeTransfer(owner(), amountClaimed);
-  }
-
-  function claimRewards() external onlyOwner {
-    ILiquidityGauge(crvRewardDistributor).claim_rewards(address(this), owner());
   }
 }
