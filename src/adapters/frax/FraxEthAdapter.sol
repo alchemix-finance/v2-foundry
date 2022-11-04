ad0993446511bf51e6938ad7bdc9f8fb01c934e8
hesnicewithit <splitthedutch@gmail.com>
2022-11-04 16:57:11 -0600

    Integration tests with harvest and liquidate

--- src/adapters/frax/FraxEthAdapter.sol
+++ src/adapters/frax/FraxEthAdapter.sol
@@ -1,30 +1,69 @@
 pragma solidity ^0.8.13;
 
 import {IllegalState} from "../../base/Errors.sol";
+import {IllegalArgument, Unauthorized} from "../../base/ErrorMessages.sol";
 
+
+import {IFraxMinter} from "../../interfaces/external/frax/IFraxMinter.sol";
+import {IFraxEth} from "../../interfaces/external/frax/IFraxEth.sol";
 import {IStakedFraxEth} from "../../interfaces/external/frax/IStakedFraxEth.sol";
+import {IStableSwap2Pool} from "../../interfaces/external/curve/IStableSwap2Pool.sol";
 import {ITokenAdapter} from "../../interfaces/ITokenAdapter.sol";
 import {IWETH9} from "../../interfaces/external/IWETH9.sol";
+
+import {MutexLock} from "../../base/MutexLock.sol";
 
 import "../../libraries/TokenUtils.sol";
 
 struct InitializationParams {
+    address alchemist;
+    address curvePool;
+    address minter;
     address token;
+    address parentToken;
     address underlyingToken;
+    uint128 curvePoolEthIndex;
+    uint128 curvePoolfrxEthIndex;
 }
 
 /// @title  FraxEthAdapter
 /// @author Alchemix Finance
-contract FraxEthAdapter is ITokenAdapter {
+contract FraxEthAdapter is ITokenAdapter, MutexLock {
     uint256 private constant MAXIMUM_SLIPPAGE = 10000;
     string public constant override version = "1.0.0";
 
+    address public immutable alchemist;
+    address public immutable curvePool;
+    address public immutable minter;
     address public immutable override token;
+    address public immutable parentToken;
     address public immutable override underlyingToken;
+    uint128 public immutable curvePoolEthIndex;
+    uint128 public immutable curvePoolfrxEthIndex;
 
     constructor(InitializationParams memory params) {
+        alchemist = params.alchemist;
+        curvePool = params.curvePool;
+        curvePoolEthIndex = params.curvePoolEthIndex;
+        curvePoolfrxEthIndex = params.curvePoolfrxEthIndex;
+        minter = params.minter;
         token = params.token;
+        parentToken = params.parentToken;
         underlyingToken = params.underlyingToken;
+    }
+
+    /// @dev Checks that the message sender is the alchemist that the adapter is bound to.
+    modifier onlyAlchemist() {
+        if (msg.sender != alchemist) {
+            revert Unauthorized("Not alchemist");
+        }
+        _;
+    }
+
+    receive() external payable {
+        if (msg.sender != underlyingToken && msg.sender != curvePool) {
+            revert Unauthorized("Payments only permitted from WETH or curve pool");
+        }
     }
 
     /// @inheritdoc ITokenAdapter
@@ -33,20 +72,45 @@ contract FraxEthAdapter is ITokenAdapter {
     }
 
     /// @inheritdoc ITokenAdapter
-    function wrap(uint256 amount, address recipient) external override returns (uint256) {
+    function wrap(uint256 amount, address recipient) external lock onlyAlchemist returns (uint256) {
         TokenUtils.safeTransferFrom(underlyingToken, msg.sender, address(this), amount);
-        TokenUtils.safeApprove(underlyingToken, token, 0);
-        TokenUtils.safeApprove(underlyingToken, token, amount);
 
-        return IStakedFraxEth(token).deposit(amount, recipient);
+        // Unwrap the WETH into ETH.
+        IWETH9(underlyingToken).withdraw(amount);
+
+        // Mint frxEth.
+        uint256 startingFraxEthBalance = IERC20(parentToken).balanceOf(address(this));
+        IFraxMinter(minter).submit{value: amount}();
+        uint256 mintedFraxEth = IERC20(parentToken).balanceOf(address(this)) - startingFraxEthBalance;
+
+        TokenUtils.safeApprove(parentToken, token, mintedFraxEth);
+        return IStakedFraxEth(token).deposit(mintedFraxEth, recipient);
     }
 
     /// @inheritdoc ITokenAdapter
-    function unwrap(uint256 amount, address recipient) external override returns (uint256) {
+    function unwrap(uint256 amount, address recipient) external lock onlyAlchemist returns (uint256) {
         TokenUtils.safeTransferFrom(token, msg.sender, address(this), amount);
 
-        IStakedFraxEth(token).withdraw(amount * this.price() / 10**TokenUtils.expectDecimals(token), recipient, address(this));
+        // Withdraw frxEth from  sfrxEth.
+        uint256 startingFraxEthBalance = IERC20(parentToken).balanceOf(address(this));
+        IStakedFraxEth(token).withdraw(amount * this.price() / 10**TokenUtils.expectDecimals(token), address(this), address(this));
+        uint256 withdrawnFraxEth = IERC20(parentToken).balanceOf(address(this)) - startingFraxEthBalance;
 
-        return TokenUtils.safeBalanceOf(underlyingToken, recipient);
+        // Swap frxEth for eth in curve.
+        TokenUtils.safeApprove(parentToken, curvePool, withdrawnFraxEth);
+        uint256 received = IStableSwap2Pool(curvePool).exchange(
+            int128(uint128(curvePoolfrxEthIndex)),
+            int128(uint128(curvePoolEthIndex)),
+            withdrawnFraxEth,
+            0                                // <- Slippage is handled upstream
+        );
+
+        // Wrap the ETH that we received from the exchange.
+        IWETH9(underlyingToken).deposit{value: received}();
+
+        // Transfer the tokens to the recipient.
+        TokenUtils.safeTransfer(underlyingToken, recipient, received);
+
+        return received;
     }
 }
--- src/test/FraxEthAdapter.t.sol
+++ src/test/FraxEthAdapter.t.sol
@@ -13,8 +13,11 @@ import {
 import {IAlchemistV2} from "../interfaces/IAlchemistV2.sol";
 import {IAlchemistV2AdminActions} from "../interfaces/alchemist/IAlchemistV2AdminActions.sol";
 import {IWhitelist} from "../interfaces/IWhitelist.sol";
+import {ITransmuterBuffer} from "../interfaces/transmuter/ITransmuterBuffer.sol";
 import {ICERC20} from "../interfaces/external/compound/ICERC20.sol";
 import {IStakedFraxEth} from "../interfaces/external/frax/IStakedFraxEth.sol";
+import {IFraxMinter} from "../interfaces/external/frax/IFraxMinter.sol";
+
 
 import {SafeERC20} from "../libraries/SafeERC20.sol";
 import {LibFuse} from "../libraries/LibFuse.sol";
@@ -26,9 +29,12 @@ contract FraxEthAdapterTest is DSTestPlus {
     address constant admin = 0x8392F6669292fA56123F71949B52d883aE57e225;
     address constant alchemistETH = 0x062Bf725dC4cDF947aa79Ca2aaCCD4F385b13b5c;
     address constant alETH = 0x0100546F2cD4C9D97f798fFC9755E47865FF7Ee6;
+    address constant curvePool = 0xa1F8A6807c402E4A15ef4EBa36528A3FED24E577;
     address constant frxEth = 0x5E8422345238F34275888049021821E8E08CAa1f;
+    address constant minter = 0xbAFA44EFE7901E04E39Dad13167D089C559c1138;
     address constant owner = 0x9e2b6378ee8ad2A4A95Fe481d63CAba8FB0EBBF9;
     address constant sfrxEth = 0xac3E018457B222d93114458476f3E3416Abbe38F;
+    address constant transmuterBuffer = 0xbc2FB245594a68c927C930FBE2d00680A8C90B9e;
     address constant wETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address constant whitelistETH = 0xA3dfCcbad1333DC69997Da28C961FF8B2879e653;
 
@@ -36,8 +42,14 @@ contract FraxEthAdapterTest is DSTestPlus {
 
     function setUp() external {
         adapter = new FraxEthAdapter(AdapterInitializationParams({
-            token:           sfrxEth,
-            underlyingToken: frxEth
+            alchemist:              alchemistETH,
+            curvePool:              curvePool,
+            curvePoolEthIndex:      0,
+            curvePoolfrxEthIndex:   1,
+            minter:                 minter,
+            token:                  sfrxEth,
+            parentToken:            frxEth,
+            underlyingToken:        wETH
         }));
 
         IAlchemistV2.YieldTokenConfig memory ytc = IAlchemistV2AdminActions.YieldTokenConfig({
@@ -47,22 +59,12 @@ contract FraxEthAdapterTest is DSTestPlus {
             creditUnlockBlocks: 7200
         });
 
-        IAlchemistV2AdminActions.UnderlyingTokenConfig memory utc = IAlchemistV2AdminActions.UnderlyingTokenConfig({
-			repayLimitMinimum: 1,
-			repayLimitMaximum: 1000,
-			repayLimitBlocks: 10,
-			liquidationLimitMinimum: 1,
-			liquidationLimitMaximum: 1000000000000000000,
-			liquidationLimitBlocks: 7200
-		});
-
         hevm.startPrank(owner);
         IWhitelist(whitelistETH).add(address(adapter));
         IWhitelist(whitelistETH).add(address(this));
-        IAlchemistV2(alchemistETH).addUnderlyingToken(frxEth, utc);
         IAlchemistV2(alchemistETH).addYieldToken(sfrxEth, ytc);
         IAlchemistV2(alchemistETH).setYieldTokenEnabled(sfrxEth, true);
-        IAlchemistV2(alchemistETH).setUnderlyingTokenEnabled(frxEth, true);
+        ITransmuterBuffer(transmuterBuffer).setFlowRate(frxEth, 300000000000000);
         hevm.stopPrank();
     }
 
@@ -71,48 +73,9 @@ contract FraxEthAdapterTest is DSTestPlus {
     }
 
     function testRoundTrip() external {
-        deal(frxEth, address(this), 1e18);
+        deal(wETH, address(this), 1e18);
 
-        SafeERC20.safeApprove(address(frxEth), address(adapter), 1e18);
-        uint256 wrapped = adapter.wrap(1e18, address(this));
-
-        uint256 underlyingValue = wrapped * adapter.price() / 10**SafeERC20.expectDecimals(sfrxEth);
-        assertGt(underlyingValue, 1e18 * 9900 / BPS /* 1% slippage */);
-
-        SafeERC20.safeApprove(adapter.token(), address(adapter), wrapped);
-        uint256 unwrapped = adapter.unwrap(wrapped, address(this));
-
-        assertGt(unwrapped, 1e18 * 9900 / BPS /* 1% slippage */);
-        assertEq(IERC20(sfrxEth).balanceOf(address(this)), 0);
-        assertApproxEq(IERC20(sfrxEth).balanceOf(address(adapter)), 0, 10);
-    }
-
-    function testRoundTrip(uint256 amount) external {
-        hevm.assume(
-            amount >= 10**SafeERC20.expectDecimals(adapter.underlyingToken()) && 
-            amount < 1000000000000000000000
-        );
-
-         deal(frxEth, address(this), amount);
-
-        SafeERC20.safeApprove(address(frxEth), address(adapter), amount);
-        uint256 wrapped = adapter.wrap(amount, address(this));
-
-        uint256 underlyingValue = wrapped * adapter.price() / 10**SafeERC20.expectDecimals(sfrxEth);
-        assertGt(underlyingValue, amount * 9900 / BPS /* 1% slippage */);
-
-        SafeERC20.safeApprove(adapter.token(), address(adapter), wrapped);
-        uint256 unwrapped = adapter.unwrap(wrapped, address(this));
-
-        assertGt(unwrapped, amount * 9900 / BPS /* 1% slippage */);
-        assertEq(IERC20(sfrxEth).balanceOf(address(this)), 0);
-        assertApproxEq(IERC20(sfrxEth).balanceOf(address(adapter)), 0, 100);
-    }
-
-    function testRoundTripIntegration() external {
-        deal(frxEth, address(this), 1e18);
-
-        SafeERC20.safeApprove(address(frxEth), address(alchemistETH), 1e18);
+        SafeERC20.safeApprove(address(wETH), address(alchemistETH), 1e18);
         uint256 wrapped = IAlchemistV2(alchemistETH).depositUnderlying(sfrxEth, 1e18, address(this), 0);
 
         uint256 underlyingValue = wrapped * adapter.price() / 10**SafeERC20.expectDecimals(sfrxEth);
@@ -124,17 +87,17 @@ contract FraxEthAdapterTest is DSTestPlus {
         assertEq(IERC20(sfrxEth).balanceOf(address(this)), 0);
         assertApproxEq(IERC20(sfrxEth).balanceOf(address(adapter)), 0, 10);
     }
-
     function testHarvest() external {
-        tip(sfrxEth, address(this), 1e18);
+        deal(wETH, address(this), 1e18);
+        deal(wETH, address(0xbeef), 1e18);
 
-        // New position
-        SafeERC20.safeApprove(sfrxEth, alchemistETH, 1e18);
-        uint256 shares = IAlchemistV2(alchemistETH).deposit(sfrxEth, 1e18, address(this));
+        SafeERC20.safeApprove(address(wETH), address(alchemistETH), 1e18);
+        IAlchemistV2(alchemistETH).depositUnderlying(sfrxEth, 1e18, address(this), 0);
         (int256 debtBefore, ) = IAlchemistV2(alchemistETH).accounts(address(this));
 
         // Roll ahead then harvest
         hevm.roll(block.number + 100000);
+        hevm.warp(block.timestamp + 100000000);
         hevm.prank(owner);
         IAlchemistV2(alchemistETH).harvest(sfrxEth, 0);
 
@@ -162,29 +125,6 @@ contract FraxEthAdapterTest is DSTestPlus {
         (uint256 sharesLeft, ) =  IAlchemistV2(alchemistETH).positions(address(this), sfrxEth);
 
         assertApproxEq(0, uint256(debtAfter), mintAmt - mintAmt * 97 / 100);
-        assertEq(shares - sharesLiquidated, sharesLeft);
-    }
-
-    function testLiquidateViaBurn() external {
-        tip(sfrxEth, address(this), 1e18);
-
-        hevm.deal(sfrxEth, 1e18);
-
-        SafeERC20.safeApprove(sfrxEth, alchemistETH, 1e18);
-        uint256 shares = IAlchemistV2(alchemistETH).deposit(sfrxEth, 1e18, address(this));
-        uint256 pps = IAlchemistV2(alchemistETH).getUnderlyingTokensPerShare(sfrxEth);
-        uint256 mintAmt = shares * pps / 1e18 / 4;
-        IAlchemistV2(alchemistETH).mint(mintAmt, address(this));
-
-        (int256 debtBefore, ) = IAlchemistV2(alchemistETH).accounts(address(this));
-
-        uint256 sharesLiquidated = IAlchemistV2(alchemistETH).liquidate(sfrxEth, shares / 4, 0);
-
-        (int256 debtAfter, ) = IAlchemistV2(alchemistETH).accounts(address(this));
-
-        (uint256 sharesLeft, ) =  IAlchemistV2(alchemistETH).positions(address(this), sfrxEth);
-
-        assertApproxEq(0, uint256(debtAfter), 10);
         assertEq(shares - sharesLiquidated, sharesLeft);
     }
 }
