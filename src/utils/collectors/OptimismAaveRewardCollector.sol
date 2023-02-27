5e904d1dac899b0a4ffb0c9f8df7cf3ee2e2ab6f
hesnicewithit <splitthedutch@gmail.com>
2023-02-27 13:55:08 -0700

    Swap Fix

--- src/utils/collectors/OptimismAaveRewardCollector.sol
+++ src/utils/collectors/OptimismAaveRewardCollector.sol
@@ -63,7 +63,7 @@ contract OptimismAaveRewardCollector is IRewardCollector {
             IVelodromeSwapRouter(swapRouter).swapExactTokensForTokens(amountRewardToken, minimumAmountOut, routes, address(this), block.timestamp);
         } else if (debtToken == 0x3E29D3A9316dAB217754d13b28646B76607c5f04) {
             // Velodrome Swap Routes: OP -> alETH
-            IVelodromeSwapRouter.route[] memory routes = new IVelodromeSwapRouter.route[](2);
+            IVelodromeSwapRouter.route[] memory routes = new IVelodromeSwapRouter.route[](1);
             routes[0] = IVelodromeSwapRouter.route(0x4200000000000000000000000000000000000042, 0x3E29D3A9316dAB217754d13b28646B76607c5f04, false);
             TokenUtils.safeApprove(rewardToken, swapRouter, amountRewardToken);
             IVelodromeSwapRouter(swapRouter).swapExactTokensForTokens(amountRewardToken, minimumAmountOut, routes, address(this), block.timestamp);
