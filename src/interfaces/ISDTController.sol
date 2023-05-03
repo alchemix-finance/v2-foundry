b29b37fef6098d04d06dfbd56abc2c5519a56e4e
hesnicewithit <splitthedutch@gmail.com>
2023-05-03 09:01:19 -0600

    Added s to function name

--- src/interfaces/ISDTController.sol
+++ src/interfaces/ISDTController.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.13;
 
 interface ISDTController {
-    function voteForGaugeWeight(address gaugeAddress, uint256 weight) external;
+    function voteForGaugeWeights(address gaugeAddress, uint256 weight) external;
 }
