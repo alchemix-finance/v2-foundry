242fcc7fb5fdedb6082e8c0782045b9f3470d2b6
hesnicewithit <splitthedutch@gmail.com>
2023-05-24 12:22:57 -0600

    Updated test for ytokengateway

--- test.sh
+++ test.sh
@@ -21,3 +21,4 @@ make test_file_block FILE=PoolAssetManager BLOCK=18418991;
 make test_file_block_optimism FILE=XTokens BLOCK=118506023;
 make test_file_block FILE=SDT BLOCK=17000429;
 make test_file_block_optimism FILE=YearnOptimism BLOCK=99207058;
+make test_file_block_optimism FILE=YTokenGateway BLOCK=99207058;
