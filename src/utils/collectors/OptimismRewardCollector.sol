775826e02121074e42e48328b30aca36e0ac1026
hesnicewithit <splitthedutch@gmail.com>
2024-03-11 19:34:29 -0600

    Consolidate

--- src/utils/collectors/OptimismRewardCollector.sol
+++ src/utils/collectors/OptimismRewardCollector.sol
@@ -32,13 +32,11 @@ contract OptimismRewardCollector is IRewardCollector {
     uint256 constant FIXED_POINT_SCALAR = 1e18;
     uint256 constant BPS = 10000;
     string public override version = "1.0.0";
-    address public alchemist;
     address public debtToken;
     address public override rewardToken;
     address public override swapRouter;
 
     constructor(InitializationParams memory params) {
-        alchemist       = params.alchemist;
         debtToken       = params.debtToken;
         rewardToken     = params.rewardToken;
         swapRouter      = params.swapRouter;
@@ -47,11 +45,13 @@ contract OptimismRewardCollector is IRewardCollector {
     function claimAndDonateRewards(address token, uint256 minimumAmountOut) external returns (uint256) {
         // Amount of reward token claimed plus any sent to this contract from grants.
         uint256 amountRewardToken = IERC20(rewardToken).balanceOf(address(this));
-
         if (amountRewardToken == 0) return 0;
+
+        address alchemist;
 
         if (debtToken == 0xCB8FA9a76b8e203D8C3797bF438d8FB81Ea3326A) {
             // Velodrome Swap Routes: OP -> USDC -> alUSD
+            alchemist = 0x10294d57A419C8eb78C648372c5bAA27fD1484af;
             IVelodromeSwapRouter.route[] memory routes = new IVelodromeSwapRouter.route[](2);
             routes[0] = IVelodromeSwapRouter.route(0x4200000000000000000000000000000000000042, 0x7F5c764cBc14f9669B88837ca1490cCa17c31607, false);
             routes[1] = IVelodromeSwapRouter.route(0x7F5c764cBc14f9669B88837ca1490cCa17c31607, 0xCB8FA9a76b8e203D8C3797bF438d8FB81Ea3326A, true);
@@ -59,6 +59,7 @@ contract OptimismRewardCollector is IRewardCollector {
             IVelodromeSwapRouter(swapRouter).swapExactTokensForTokens(amountRewardToken, minimumAmountOut, routes, address(this), block.timestamp);
         } else if (debtToken == 0x3E29D3A9316dAB217754d13b28646B76607c5f04) {
             // Velodrome Swap Routes: OP -> alETH
+            alchemist = 0xe04Bb5B4de60FA2fBa69a93adE13A8B3B569d5B4;
             IVelodromeSwapRouter.route[] memory routes = new IVelodromeSwapRouter.route[](1);
             routes[0] = IVelodromeSwapRouter.route(0x4200000000000000000000000000000000000042, 0x3E29D3A9316dAB217754d13b28646B76607c5f04, false);
             TokenUtils.safeApprove(rewardToken, swapRouter, amountRewardToken);
