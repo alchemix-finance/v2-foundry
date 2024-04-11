82f6434ff2c5337efa4eee8c82f7a64ded78e7b1
hesnicewithit <splitthedutch@gmail.com>
2024-04-11 01:10:12 -0600

    added reward router access check

--- src/test/OptimismGrantDistribution.t.sol
+++ src/test/OptimismGrantDistribution.t.sol
@@ -18,7 +18,7 @@ contract OptimismGrantDistribution is DSTestPlus {
     address constant whitelist = 0xc5fE32e46fD226364BFf7A035e8Ca2aBE390a68f;
 
     function setUp() external {
-        OptimismRewardCollector collector = new OptimismRewardCollector(InitializationParams(0xe04Bb5B4de60FA2fBa69a93adE13A8B3B569d5B4, 0x3E29D3A9316dAB217754d13b28646B76607c5f04, 0x4200000000000000000000000000000000000042, 0xa062aE8A9c5e11aaA026fc2670B0D65cCc8B2858));
+        OptimismRewardCollector collector = new OptimismRewardCollector(InitializationParams(0xe04Bb5B4de60FA2fBa69a93adE13A8B3B569d5B4, 0x3E29D3A9316dAB217754d13b28646B76607c5f04, 0x6e39B07db3A0C7ce434Ce10335BB8BB20B7FEb48, 0x4200000000000000000000000000000000000042, 0xa062aE8A9c5e11aaA026fc2670B0D65cCc8B2858));
 
         vm.startPrank(0xC224bf25Dcc99236F00843c7D8C4194abE8AA94a);
         IRewardRouter(rewardRouter).setRewardCollectorAddress(0xE62DDa84e579e6A37296bCFC74c97349D2C59ce3, address(collector));
