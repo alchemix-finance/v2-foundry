e3b5b927da168b8fd4935f7b35f058c117decddf
hesnicewithit <splitthedutch@gmail.com>
2024-06-07 17:32:40 -0400

    Arb price estimates

--- src/interfaces/external/ramses/ISwapRouter.sol
+++ src/interfaces/external/ramses/ISwapRouter.sol
@@ -24,3 +24,42 @@ interface ISwapRouter {
 
   function execute(bytes calldata commands, bytes[] calldata inputs) external;
 }
+
+interface ISwapRouterv2 {
+  function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    route[] calldata routes,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+}
+
+interface V2Pool {
+  function getAmountOut(uint256 amountIn, address tokenIn) external view returns (uint256 amountOut);
+}
+
+interface RamsesQuote {
+  function quoteExactInputSingleV3(QuoteExactInputSingleV3Params memory params)
+    external
+    returns (
+      uint256 amountOut, 
+      uint160 sqrtPriceX96After, 
+      uint32 initializedTicksCrossed, 
+      uint256 gasEstimate
+    );
+}
+
+struct QuoteExactInputSingleV3Params {
+    address tokenIn;
+    address tokenOut;
+    uint256 amountIn;
+    uint24 fee;
+    uint160 sqrtPriceLimitX96;
+}
+
+struct route {
+    address from;
+    address to;
+    bool stable;
+}
--- src/test/ArbitrumGrantDistribution.t.sol
+++ src/test/ArbitrumGrantDistribution.t.sol
@@ -16,14 +16,18 @@ import {RewardRouter} from "../utils/RewardRouter.sol";
 contract ArbitrumGrantDistribution is DSTestPlus {
     address constant admin = 0x886FF7a2d46dcc2276e2fD631957969441130847;
     address constant alchemist = 0xb46eE2E4165F629b4aBCE04B7Eb4237f951AC66F;
+    address constant alchemistETH = 0x654e16a0b161b150F5d1C8a5ba6E7A7B7760703A;
     address constant whitelist = 0xda94B6536E9958d63229Dc9bE4fa654Ad52921dB;
+    address constant whitelistETH = 0x6996b41c369D3175F18D16ba14952F8C89665710;
 
     RewardRouter router = new RewardRouter();
     ArbitrumRewardCollector collector = new ArbitrumRewardCollector(InitializationParams(alchemist, 0xCB8FA9a76b8e203D8C3797bF438d8FB81Ea3326A, address(router), 0x912CE59144191C1204E64559FE8253a0e49E6548, 0x5C23A419436F6D65c506Bbc329A783ADB3335e3C));
+    ArbitrumRewardCollector collectorETH = new ArbitrumRewardCollector(InitializationParams(alchemistETH, 0x17573150d67d820542EFb24210371545a4868B03, address(router), 0x912CE59144191C1204E64559FE8253a0e49E6548, 0x5C23A419436F6D65c506Bbc329A783ADB3335e3C));
 
     function setUp() external {
         deal(0x912CE59144191C1204E64559FE8253a0e49E6548, address(router), 10e18);
         router.addVault(0x248a431116c6f6FCD5Fe1097d16d0597E24100f5, address(collector), 10e18, 604800);
+        router.addVault(0x5979D7b546E38E414F7E9822514be443A4800529, address(collectorETH), 10e18, 604800);
         router.setHarvester(0x52E4C31933B466CD8A7cb0aAb819abAF7BE7Fc0e);
 
         collector.setRewardRouter(address(router));
@@ -33,6 +37,7 @@ contract ArbitrumGrantDistribution is DSTestPlus {
 
         vm.startPrank(admin);
         IWhitelist(whitelist).add(address(collector));
+        IWhitelist(whitelistETH).add(address(collectorETH));
         vm.stopPrank();
     }
 
@@ -43,10 +48,17 @@ contract ArbitrumGrantDistribution is DSTestPlus {
         assertEq(IERC20(0x912CE59144191C1204E64559FE8253a0e49E6548).balanceOf(address(router)) , 0);
     }
 
-    // function testDistributePartial() external {
-    //     vm.warp(block.timestamp + 302400);
-    //     vm.prank(0x10388c006a356eF584d32A314e70D0E62CfCABeE);
-    //     IAlchemixHarvester(0x52E4C31933B466CD8A7cb0aAb819abAF7BE7Fc0e).harvest(0xb46eE2E4165F629b4aBCE04B7Eb4237f951AC66F, 0x248a431116c6f6FCD5Fe1097d16d0597E24100f5);
-    //     assertEq(IERC20(0x912CE59144191C1204E64559FE8253a0e49E6548).balanceOf(address(router)) , 500e18);
-    // }
+    function testDistributeETH() external {
+        vm.warp(block.timestamp + 604800);
+        hevm.prank(0x52E4C31933B466CD8A7cb0aAb819abAF7BE7Fc0e);
+        router.distributeRewards(0x5979D7b546E38E414F7E9822514be443A4800529);
+        assertEq(IERC20(0x912CE59144191C1204E64559FE8253a0e49E6548).balanceOf(address(router)) , 0);
+    }
+
+    function testDistributePartial() external {
+        vm.warp(block.timestamp + 302400);
+        vm.prank(0x10388c006a356eF584d32A314e70D0E62CfCABeE);
+        IAlchemixHarvester(0x52E4C31933B466CD8A7cb0aAb819abAF7BE7Fc0e).harvest(0xb46eE2E4165F629b4aBCE04B7Eb4237f951AC66F, 0x248a431116c6f6FCD5Fe1097d16d0597E24100f5);
+        assertEq(IERC20(0x912CE59144191C1204E64559FE8253a0e49E6548).balanceOf(address(router)) , 5e18);
+    }
 }
