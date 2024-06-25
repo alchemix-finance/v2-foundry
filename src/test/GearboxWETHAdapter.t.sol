04b2d214611472a87a552fc7372489ad327a9cfa
TheGreatGildo <scupytrooples@mail.alchemix.fi>
2024-06-25 09:40:27 +0800

    sanity test

--- src/test/GearboxWETHAdapter.t.sol
+++ src/test/GearboxWETHAdapter.t.sol
@@ -14,7 +14,7 @@ import { IAlchemistV2AdminActions } from "../interfaces/alchemist/IAlchemistV2Ad
 contract GearboxWETHAdaptorTest is DSTestPlus {
 	uint256 constant BPS = 10000;
 	address constant admin = 0x886FF7a2d46dcc2276e2fD631957969441130847;
-  //todo
+	//todo
 	address constant whitelistWETHAddress = 0x6996b41c369D3175F18D16ba14952F8C89665710;
 
 	IAlchemistV2 constant alchemist = IAlchemistV2(0x654e16a0b161b150F5d1C8a5ba6E7A7B7760703A);
@@ -33,7 +33,7 @@ contract GearboxWETHAdaptorTest is DSTestPlus {
 		});
 		hevm.startPrank(admin);
 		alchemist.addYieldToken(address(dWETH), ytc);
-    alchemist.setYieldTokenEnabled(address(dWETH), true);
+		alchemist.setYieldTokenEnabled(address(dWETH), true);
 		alchemist.setTokenAdapter(address(dWETH), address(adapter));
 		IWhitelist(whitelistWETHAddress).add(address(this));
 		alchemist.setMaximumExpectedValue(address(dWETH), 1000000000e18);
@@ -52,7 +52,12 @@ contract GearboxWETHAdaptorTest is DSTestPlus {
 		uint256 underlyingValue = (shares * adapter.price()) / 10 ** SafeERC20.expectDecimals(address(dWETH));
 		assertGt(underlyingValue, (1e18 * 9990) / BPS);
 
-		uint256 unwrapped = alchemist.withdrawUnderlying(address(dWETH), shares, address(this), (shares * 9990) / 10000);
+		uint256 unwrapped = alchemist.withdrawUnderlying(
+			address(dWETH),
+			shares,
+			address(this),
+			(shares * 9990) / 10000
+		);
 
 		uint256 endBalance = dWETH.balanceOf(address(alchemist));
 
@@ -62,62 +67,57 @@ contract GearboxWETHAdaptorTest is DSTestPlus {
 		assertApproxEq(endBalance - startingBalance, 0, 10);
 		console2.log("startingBalance", startingBalance);
 		console2.log("endBalance", endBalance);
-
 	}
 
-    function testHarvest() external {
-        deal(address(dWETH), address(this), 1e18);
+	function testHarvest() external {
+		deal(address(dWETH), address(this), 1e18);
 
+		// New position
+		SafeERC20.safeApprove(address(dWETH), address(alchemist), 1e18);
+		uint256 shares = alchemist.deposit(address(dWETH), 1e18, address(this));
+		(int256 debtBefore, ) = alchemist.accounts(address(this));
+		uint256 priceBefore = adapter.price();
 
-        // New position
-        SafeERC20.safeApprove(address(dWETH), address(alchemist), 1e18);
-        uint256 shares = alchemist.deposit(address(dWETH), 1e18, address(this));
-        (int256 debtBefore, ) = alchemist.accounts(address(this));
-				uint256 priceBefore = adapter.price();
+		// Roll ahead then harvest
+		hevm.roll(block.number + 1000);
+		hevm.warp(block.timestamp + 16532);
+		hevm.prank(admin);
+		alchemist.harvest(address(dWETH), 0);
+		// Roll ahead one block then check credited amount
+		uint256 priceAfter = adapter.price();
 
-        // Roll ahead then harvest
-        hevm.roll(block.number + 1000);
-        hevm.warp(block.timestamp + 16532);
-        hevm.prank(admin);
-        alchemist.harvest(address(dWETH), 0);
-        // Roll ahead one block then check credited amount
-        hevm.roll(block.number + 1);
-        (int256 debtAfter, ) = alchemist.accounts(address(this));
-				uint256 priceAfter = adapter.price();
+		hevm.roll(block.number + 10000);
+		(int256 debtAfter, ) = alchemist.accounts(address(this));
 
-        assertGt(debtBefore, debtAfter);
-				console2.log("debtBefore", debtBefore);
-				console2.log("debtAfter", debtAfter);
-				console2.log("price before", priceBefore);
-				console2.log("price after", priceAfter);
-				console2.log("price difference", priceAfter - priceBefore);
+		assertGt(debtBefore, debtAfter);
+		console2.log("debtBefore", debtBefore);
+		console2.log("debtAfter", debtAfter);
+		console2.log("price before", priceBefore);
+		console2.log("price after", priceAfter);
+		console2.log("price difference", priceAfter - priceBefore);
+	}
 
-    }
+	function testLiquidate() external {
+		deal(address(dWETH), address(this), 1e18);
 
-    function testLiquidate() external {
-        deal(address(dWETH), address(this), 1e18);
+		SafeERC20.safeApprove(address(dWETH), address(alchemist), 1e18);
+		uint256 shares = alchemist.deposit(address(dWETH), 1e18, address(this));
+		uint256 pps = alchemist.getUnderlyingTokensPerShare(address(dWETH));
+		uint256 mintAmt = (shares * pps) / 1e18 / 4;
+		alchemist.mint(mintAmt, address(this));
 
+		(int256 debtBefore, ) = alchemist.accounts(address(this));
 
-        SafeERC20.safeApprove(address(dWETH), address(alchemist), 1e18);
-        uint256 shares = alchemist.deposit(address(dWETH), 1e18, address(this));
-        uint256 pps = alchemist.getUnderlyingTokensPerShare(address(dWETH));
-        uint256 mintAmt = shares * pps / 1e18 / 4;
-        alchemist.mint(mintAmt, address(this));
+		uint256 sharesLiquidated = alchemist.liquidate(address(dWETH), shares / 4, (mintAmt * 97) / 100);
 
-        (int256 debtBefore, ) = alchemist.accounts(address(this));
+		(int256 debtAfter, ) = alchemist.accounts(address(this));
 
-        uint256 sharesLiquidated = alchemist.liquidate(address(dWETH), shares / 4, mintAmt * 97 / 100);
+		(uint256 sharesLeft, ) = alchemist.positions(address(this), address(dWETH));
 
-        (int256 debtAfter, ) = alchemist.accounts(address(this));
-
-        (uint256 sharesLeft, ) =  alchemist.positions(address(this), address(dWETH));
-
-        assertApproxEq(0, uint256(debtAfter), mintAmt - mintAmt * 97 / 100);
-        assertEq(shares - sharesLiquidated, sharesLeft);
-				console2.log("shares", shares);
-				console2.log("sharesLiquidated", sharesLiquidated);
-				console2.log("sharesLeft", sharesLeft);
-    }
-
+		assertApproxEq(0, uint256(debtAfter), mintAmt - (mintAmt * 97) / 100);
+		assertEq(shares - sharesLiquidated, sharesLeft);
+		console2.log("shares", shares);
+		console2.log("sharesLiquidated", sharesLiquidated);
+		console2.log("sharesLeft", sharesLeft);
+	}
 }
-
